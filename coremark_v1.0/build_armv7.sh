78eedf4e6cb32ef831f3a74167ede5a8da4de472
hawkin <mrpzhanghongjun@163.com>
2018-07-10 13:33:13 +0800

    modify buidl script

--- coremark_v1.0/build_armv7.sh
+++ coremark_v1.0/build_armv7.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-runarmv7hf
+export ARCH=arm
+export LOAD_ADDR=0x10008000
+export CROSS_COMPILE=arm-linux-gnueabihf-
+export PATH=/opt/hawkin/toolchain/arm-linux-guneabihf/gcc-linaro-6.3.1-2017.05-x86_64_arm-linux-gnueabihf/bin/:$PATH
+
 make PORT_DIR=armv7 PORT_CFLAGS="-O3 -march=armv7-a -mfloat-abi=hard -mfpu=neon -mtune=cortex-a7 -funroll-all-loops --param max-inline-insns-auto=300 -static -DMULTITHREAD=4 -DUSE_FORK=1"
 
--- coremark_v1.0/build_armv8.sh
+++ coremark_v1.0/build_armv8.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-runarmv864
+export ARCH=arm64
+export LOADADDR=0x80008000
+export CROSS_COMPILE=aarch64-linux-gnu-
+export PATH=/opt/hawkin/toolchain/aarch64-linux-gnu/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu/bin/:$PATH
 
 make PORT_DIR=armv8 PORT_CFLAGS="-O3 -funroll-all-loops --param max-inline-insns-auto=550 -DMULTITHREAD=4 -DUSE_FORK=1" 
